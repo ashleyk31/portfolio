0a10a9725a71a355102d2aae7389e7864604434b
Ashley Kang <142348207+ashleyk31@users.noreply.github.com>
2025-11-19 23:27:25 -0500

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,8 +1,28 @@
-
+// ========================================================
+// ========================================================
+// hamburger menu script
 const hamMenu = document.querySelector('.ham-menu');
 const offScreenMenu = document.querySelector('.off-screen');
 
 hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
+});
+
+// ========================================================
+// ========================================================
+// nav bar toggle
+const toggleBtn = document.getElementById("menu-toggle");
+const sidebar = document.querySelector(".sidebar");
+
+toggleBtn.addEventListener("click", () => {
+    const isOpen = sidebar.style.display === "block";
+
+    if (isOpen) {
+        sidebar.style.display = "none";
+        toggleBtn.src = "assets/view.png";
+    } else {
+        sidebar.style.display = "block";
+        toggleBtn.src = "assets/hide.png";
+    }
 });
